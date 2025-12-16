84594988ba605621f23df238b3ccc512dc511f04
Vladimir Kovalchuk <vladimir.kovalchuk@external.danone.com>
2025-12-16 15:05:00 +0100

    Task1 done

--- task/1-task-models.py
+++ task/1-task-models.py
@@ -10,9 +10,25 @@ from task.app.main import run
 # - gpt-4o
 # - claude-3-7-sonnet@20250219
 # - gemini-2.5-pro
+QUESTION = "What LLMs can do?"
 
-run(
-    deployment_name='INSERT_DEPLOYMENT_NAME',
+# WHY  "content_filter_result": {"error": {"code": "content_filter_error", "message": "The contents are not filtered"}},
+run(user_input=QUESTION,
+    deployment_name='gpt-4o',
+    print_request=False, # Switch to False if you do not want to see the request in console
+    print_only_content=False, # Switch to True if you want to see only content from response
+)
+run(user_input=QUESTION,
+    deployment_name='gemini-2.5-pro',
+    print_request=False, # Switch to False if you do not want to see the request in console
+    print_only_content=False, # Switch to True if you want to see only content from response
+)
+
+# claude-3-7-sonnet@20250219 FAILS
+# {"error":{"message":"Access denied","display_message":"Access denied","code":"403"}}
+
+run(user_input=QUESTION,
+    deployment_name='claude-sonnet-4@20250514',
     print_request=False, # Switch to False if you do not want to see the request in console
     print_only_content=False, # Switch to True if you want to see only content from response
 )
--- task/app/main.py
+++ task/app/main.py
@@ -5,9 +5,12 @@ from task.models.role import Role
 
 DEFAULT_SYSTEM_PROMPT = "You are an assistant who answers concisely and informatively."
 DIAL_ENDPOINT = "https://ai-proxy.lab.epam.com/openai/deployments/{model}/chat/completions"
+# DIAL_ENDPOINT = "https://ai-proxy.lab.epam.com"
+
 
 
 def run(
+        user_input: str,
         deployment_name: str,
         print_request: bool = True,
         print_only_content: bool = False,
@@ -20,9 +23,9 @@ def run(
     conversation = Conversation()
     conversation.add_message(Message(Role.SYSTEM, DEFAULT_SYSTEM_PROMPT))
 
-    print("Type your question or 'exit' to quit.")
+    # print("Type your question or 'exit' to quit.")
     while True:
-        user_input = input("> ").strip()
+        # user_input = input("> ").strip()
     
         if user_input.lower() == "exit":
             print("Exiting the chat. Goodbye!")
@@ -30,7 +33,7 @@ def run(
     
         conversation.add_message(Message(Role.USER, user_input))
 
-        print("AI:")
+        print("AI (" + deployment_name + "): ", end="")
         ai_message = client.get_completion(
             messages=conversation.get_messages(),
             print_request=print_request,
@@ -38,3 +41,4 @@ def run(
             **kwargs
         )
         conversation.add_message(ai_message)
+        break
